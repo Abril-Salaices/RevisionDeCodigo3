b0128364de9bd5cf8eec849d13f8681cddf951d4
Abril Salaices <abril.salmed@gmail.com>
2025-07-10 11:03:31 -0600

    Agrega revisión al codigo html

--- filtro.js
+++ filtro.js
@@ -8,29 +8,35 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.getElementById("lista-de-productos")
 const $i = document.querySelector('.input');
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
 
-  var ti = document.createElement("p")
+/*Hace falta la funcion mostrar productos*/
+function displayProductos  (lista) {
+  li.innerHTML= "";
+
+
+/*Se cambia el tipo de variables "var"*/
+for (let i = 0; i < productos.length; i++) {
+  let d = document.createElement("div");
+  d.classList.add("producto");
+
+  let ti = document.createElement("p")
   ti.classList.add("titulo")
   ti.textContent = productos[i].nombre
   
-  var imagen = document.createElement("img");
+ let imagen = document.createElement("img");
   imagen.setAttribute('src', productos[i].img);
 
   d.appendChild(ti)
   d.appendChild(imagen)
-
   li.appendChild(d)
 }
+}
+displayProductos(productos);
 
-displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
-
 botonDeFiltro.onclick = function() {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
@@ -41,14 +47,14 @@ botonDeFiltro.onclick = function() {
   const productosFiltrados = filtrado(productos, texto );
 
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
+    let d = document.createElement("div")
     d.classList.add("producto")
   
-    var ti = document.createElement("p")
+    let ti = document.createElement("p")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
     
-    var imagen = document.createElement("img");
+  let imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
   
     d.appendChild(ti)
